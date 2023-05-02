8b6f598572ce29d58d6945d35244658aff147126
David Calaiò <davidcjr11@gmail.com>
2023-05-02 17:34:38 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script>
-export default {
+import Header from '../src/components/Header.vue'
+
+export default{
+  components:{
+    Header,
+  }
   
 }
 </script>
 
 <template>
-  
+  <Header />
+
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @use '../scss/main.scss'
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    name: 'Header'
+
+    
+}
+</script>
+
+<template>
+  <header>
+    <div class="container d-flex align-items-center h-100">
+        <h1>Yu-Gi-Oh API</h1>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+    header{
+        height: 70px;
+        background-color: white;
+    }
+
+</style>
